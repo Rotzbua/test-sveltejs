e890c813ece4a7d6a52e900f90dfef880c70f6fb
Rotzbua <Rotzbua@users.noreply.github.com>
2024-11-06 23:28:28 +0100

    f

--- eslint.config.js
+++ eslint.config.js
@@ -4,27 +4,27 @@ import globals from 'globals';
 import ts from 'typescript-eslint';
 
 export default ts.config(
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  ...svelte.configs["flat/recommended"],
-  {
-    languageOptions: {
-	  globals: {
-	    ...globals.browser,
-	    ...globals.node
-	  }
-	}
-  },
-  {
-    files: ["**/*.svelte"],
-
-    languageOptions: {
-	  parserOptions: {
-	    parser: ts.parser
-	  }
-	}
-  },
-  {
-    ignores: ["build/", ".svelte-kit/", "dist/"]
-  }
+    js.configs.recommended,
+    ...ts.configs.recommended,
+    ...svelte.configs["flat/recommended"],
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node
+            }
+        }
+    },
+    {
+        files: ["**/*.svelte"],
+        
+        languageOptions: {
+            parserOptions: {
+                parser: ts.parser
+            }
+        }
+    },
+    {
+        ignores: ["build/", ".svelte-kit/", "dist/"]
+    }
 );
